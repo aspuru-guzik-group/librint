83ffe5d6a91a12ac7d154c4bc98c2e335c83f498
AA <mind33eye@gmail.com>
2025-04-10 22:20:30 -0400

    added test for primal function in time.py as well as .gitignore

--- python/time.py
+++ python/time.py
@@ -14,9 +14,14 @@ from pyscfad import gto, scf
 from basis_set_exchange import get_basis
 
 basis = "sto-3g"
-geo = "HF"
+# basis = "6-31g*"
+geo = "CH4"
+# geo = "C6H6"
 
 MAX_ITER=4000
+
+do_test_gradient = False
+do_test_primal = True
 
 jax.config.update("jax_platform_name", "cpu")
 
@@ -78,6 +83,20 @@ geometries = {
         ('H', 1, (0.0,0.0,1.4624207)),
         ('Li', 3, (0.0,0.0,0.1))
     ],
+    'C6H6': [
+    ('C', 6,      ( -1.8771137684,     -1.8237635912,      2.2841118024)),
+    ('C', 6,      ( -4.5038328925,     -1.7156712646,      2.2625689261)),
+    ('C', 6,      ( -5.7931929345,     -1.5410605833,     -0.0215428763)),
+    ('C', 6,      ( -4.4564007702,     -1.4743532559,     -2.2843007750)),
+    ('C', 6,      ( -1.8298706186,     -1.5826345550,     -2.2625689261)),
+    ('C', 6,      ( -0.5403216040,     -1.7572452364,      0.0215428763)),
+    ('H', 1,      ( -0.8625198851,     -1.9613356431,      4.0818081353)),
+    ('H', 1,      ( -5.5562212955,     -1.7682056471,      4.0426908074)),
+    ('H', 1,      ( -7.8601752208,     -1.4562118864,     -0.0385504102)),
+    ('H', 1,      ( -5.4711836260,     -1.3369701765,     -4.0816191627)),
+    ('H', 1,      ( -0.7780491334,     -1.5304781177,     -4.0430687526)),
+    ('H', 1,      (  1.5264717097,     -1.8422829059,      0.0387393828))
+    ],
 }
 
 
@@ -104,7 +123,7 @@ def test_librpyscf():
     return gradient
 
 def test_jax():
-    # mol_jax = build_mol(atom, charge, basis)
+    mol_jax = build_mol(atom, charge, basis)
     E, grad = value_and_grad(hf_energy)(mol_jax)
     return grad.coords, grad.ctr_coeff, grad.exp
 
@@ -116,22 +135,28 @@ def test_grad():
     gradient = librpyscf.grad(mol_rpyscf, P)
     return gradient
 
+if do_test_gradient:
+    # Timing the functions
+    n_runs = 10  # Number of runs for averaging
 
-# Timing the functions
-n_runs = 10  # Number of runs for averaging
+    time_librpyscf = timeit.timeit(test_librpyscf, number=n_runs)
+    time_jax = timeit.timeit(test_jax, number=n_runs)
+    time_analytical = timeit.timeit(test_analytical, number=n_runs)
+    time_grad = timeit.timeit(test_grad, number=n_runs)
 
-time_librpyscf = timeit.timeit(test_librpyscf, number=n_runs)
-time_jax = timeit.timeit(test_jax, number=n_runs)
-time_analytical = timeit.timeit(test_analytical, number=n_runs)
-time_grad = timeit.timeit(test_grad, number=n_runs)
+    # Print results
+    print(f"{geo} {basis}")
+    print(f"Average time for librpyscf.denergy: {time_librpyscf / n_runs:.6f} seconds per run")
+    print(f"Average time for jax.value_and_grad: {time_jax / n_runs:.6f} seconds per run")
+    print(f"Average time for librpyscf.analytical: {time_analytical / n_runs:.6f} seconds per run")
+    print(f"Average time for librpyscf.grad: {time_grad / n_runs:.6f} seconds per run")
 
-# Print results
-print(f"{geo} {basis}")
-print(f"Average time for librpyscf.denergy: {time_librpyscf / n_runs:.6f} seconds per run")
-print(f"Average time for jax.value_and_grad: {time_jax / n_runs:.6f} seconds per run")
-print(f"Average time for librpyscf.analytical: {time_analytical / n_runs:.6f} seconds per run")
-print(f"Average time for librpyscf.grad: {time_grad / n_runs:.6f} seconds per run")
 
+    # # print grads
+    # grad_jax = test_jax()
+    # grad_librpyscf = test_librpyscf()
+    # print("Gradient from JAX:", grad_jax)
+    # print("Gradient from librpyscf:", grad_librpyscf)
 
 
 
@@ -156,3 +181,37 @@ print(f"Average time for librpyscf.grad: {time_grad / n_runs:.6f} seconds per ru
 #     f.write(f"librpyscf.denergy: {time_librpyscf / n_runs:.6f} seconds per run\n")
 #     f.write(f"jax.value_and_grad: {time_jax / n_runs:.6f} seconds per run\n")
 #     f.write(f"jax / librpyscf: {(time_jax / n_runs)/(time_librpyscf / n_runs):.6f}\n\n")
+
+
+
+# AA: stuff below is mislabeled as JAX, but actually calling PySCF
+# reason was to test the int1e_ovlp JAX derivative, which turned out to be not easy
+def jax_int1e_ovlp(mol_jax):
+    return mol.intor('int1e_ovlp')
+    # return mol.intor('int2e')
+    # return mol_jax.intor('int2e')
+
+
+def test_jax():
+    # mol_jax = build_mol(atom, charge, basis)
+    # return value_and_grad(jax_int1e_ovlp)(mol_jax)
+    return jax_int1e_ovlp(mol_jax)
+
+
+import libcscf
+
+def test_librpyscf():
+    atm, bas, env, nelec = librpyscf.prep(mol_rpyscf)
+    # mol_rpyscf = pyscf.gto.M(atom=atom, 
+    #                       basis=basis)
+    return libcscf.int1e(atm, bas, env, 'ovlp')
+    # return libcscf.int2e(atm, bas, env)
+
+# # time
+if do_test_primal:
+    n_runs = 100
+    time_jax = timeit.timeit(test_jax, number=n_runs)
+    time_librpyscf = timeit.timeit(test_librpyscf, number=n_runs)
+
+    print(f"Average time for jax.int1e_ovlp : {time_jax / n_runs:.6f} seconds per run")
+    print(f"Average time for librpyscf.int1e: {time_librpyscf / n_runs:.6f} seconds per run")
