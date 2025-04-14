d719f87e78c50006477150ded82270e01428bd70
jpmedinagl <jpmedinagl@gmail.com>
2025-04-14 12:43:18 -0400

    testing opt with different molecules

---
+++ grad/h2_two.rs
@@ -0,0 +1,96 @@
+use std::io;
+use std::ptr;
+use std::time::Instant;
+
+use librint::cint_bas::CINTcgto_cart;
+use librint::cint1e::cint1e_ovlp_cart;
+use librint::cint2e::cint2e_cart;
+use librint::cint2e::cint2e_cart_optimizer;
+
+use librint::cint::CINTOpt;
+
+pub const ATM_SLOTS: usize = 6;
+pub const BAS_SLOTS: usize = 8;
+
+fn main() -> io::Result<()> {
+    const natm: usize = 2;
+    const nbas: usize = 2;
+
+    let mut atm_arr: [i32; natm * ATM_SLOTS] = [1, 20, 1, 23, 0, 0, 1, 24, 1, 27, 0, 0];
+    let mut bas_arr: [i32; nbas * BAS_SLOTS] = [0, 0, 3, 1, 0, 28, 31, 0, 1, 0, 3, 1, 0, 28, 31, 0];
+    let mut env_arr: [f64; 34] = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 
+        -1.5117809, 0., 0., 0., 1.5117809, 0., 3.42525091, 0.62391373, 0.1688554, 0.98170675, 0.94946401, 0.29590645];
+
+    let mut shls_arr: [i32; 4] = [0, 0, 0, 0];
+
+    let mut opt_ptr: *mut CINTOpt = ptr::null_mut();
+
+    unsafe {
+        cint2e_cart_optimizer(
+            &mut opt_ptr as *mut *mut CINTOpt,  // double pointer
+            atm_arr.as_mut_ptr(),
+            natm as i32,
+            bas_arr.as_mut_ptr(),
+            nbas as i32,
+            env_arr.as_mut_ptr(),
+        );
+    }
+
+    // unsafe {
+    //     if !opt_ptr.is_null() {
+    //         let opt: &CINTOpt = &*opt_ptr;
+    //         println!("nbas: {}", opt.nbas);
+    //         println!("index_xyz_array: {:?}", opt.index_xyz_array);
+    //         // ... same for other fields
+    //     }
+    // }
+
+
+    let mut buf;
+
+	println!("buf");
+    for i in 0..nbas {
+        for j in 0..nbas {
+            for k in 0..nbas {
+                for l in 0..nbas {
+                    shls_arr[0] = i as i32;
+                    shls_arr[1] = j as i32;
+                    shls_arr[2] = k as i32;
+                    shls_arr[3] = l as i32;
+                    
+                    let di = unsafe { CINTcgto_cart(i, &bas_arr) };
+                    let dj = unsafe { CINTcgto_cart(j, &bas_arr) };
+                    let dk = unsafe { CINTcgto_cart(k, &bas_arr) };
+                    let dl = unsafe { CINTcgto_cart(l, &bas_arr) };
+
+                    buf = vec![0.0; (di * dj * dk * dl) as usize];
+
+                    
+                    let start = Instant::now();
+                    unsafe {
+                        cint2e_cart(
+                            buf.as_mut_ptr(),
+                            shls_arr.as_mut_ptr(),
+                            atm_arr.as_mut_ptr(),
+                            natm as i32,
+                            bas_arr.as_mut_ptr(),
+                            nbas as i32,
+                            env_arr.as_mut_ptr(),
+                            opt_ptr,
+                        );
+                    }
+                    let duration = start.elapsed();
+                    
+                    for i in 0..((di * dj * dk * dl) as usize) {
+                        print!("{} ", buf[i]);
+                    }
+
+                    println!("time: {:?} ", duration);
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+    Ok(())
+}
