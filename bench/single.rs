0bfb68cd87048286b6df1daba569a75f0af484ae
jpmedinagl <jpmedinagl@gmail.com>
2025-06-09 11:07:39 -0400

    primal comp exp

--- bench/single.rs
+++ bench/single.rs
@@ -1,28 +1,70 @@
 use std::io;
+use std::env;
 use std::time::{Instant, Duration};
 
-use librint::cint_bas::CINTcgto_cart;
-use librint::cint1e::cint1e_ovlp_cart;
-use librint::cint1e::cint1e_nuc_cart;
-use librint::cint1e::int1e_nuc_cart;
-use librint::cint::CINTOpt;
 use librint::utils::{nmol, read_basis};
 
+use librint::cint_bas::CINTcgto_cart;
+use librint::cint1e::cint1e_nuc_cart;
 use librint::cint2e::cint2e_cart;
 
 pub const ATM_SLOTS: usize = 6;
 pub const BAS_SLOTS: usize = 8;
 
-fn main() -> io::Result<()> {
-    let mut atm: Vec<i32> = Vec::new();
-    let mut bas: Vec<i32> = Vec::new();
-    let mut env: Vec<f64> = Vec::new();
+fn nuc(
+    atm: &mut Vec<i32>,
+    natm: usize,
+    bas: &mut Vec<i32>,
+    nbas: usize,
+    env: &mut Vec<f64>,
+) -> Duration {
+    let mut shls: [i32; 4] = [0, 0, 0, 0];
 
-    let path = "/h/332/jpmedina/libcint/molecules/c6h6/631g.txt";
-    read_basis(&path, &mut atm, &mut bas, &mut env);
+    let mut buf;
+    let mut di;
+    let mut dj;
 
-    let (natm, nbas) = nmol(&mut atm, &mut bas);
+    let start_total = Instant::now();
+    for i in 0..nbas {
+        for j in 0..nbas {
+            shls[0] = i as i32;
+            shls[1] = j as i32;
+            
+            di = CINTcgto_cart(i, &bas);
+            dj = CINTcgto_cart(j, &bas);
 
+            buf = vec![0.0; (di * dj) as usize];
+            
+            // let start = Instant::now();
+            unsafe {
+                cint1e_nuc_cart(
+                    buf.as_mut_ptr(),
+                    shls.as_mut_ptr(),
+                    atm.as_mut_ptr(),
+                    natm as i32,
+                    bas.as_mut_ptr(),
+                    nbas as i32,
+                    env.as_mut_ptr(),
+                    std::ptr::null_mut(),
+                );
+            }
+            // println!("{} {} {:?}", i, j, start.elapsed());
+
+        }
+    }
+    
+    let duration_total = start_total.elapsed();
+
+    return duration_total;
+}
+
+fn rep(
+    atm: &mut Vec<i32>,
+    natm: usize,
+    bas: &mut Vec<i32>,
+    nbas: usize,
+    env: &mut Vec<f64>,
+) -> Duration {
     let mut shls: [i32; 4] = [0, 0, 0, 0];
 
     let mut buf;
@@ -31,9 +73,7 @@ fn main() -> io::Result<()> {
     let mut dk;
     let mut dl;
 
-	println!("buf");
     let start_total = Instant::now();
-
     for i in 0..nbas {
         for j in 0..nbas {
             for k in 0..nbas {
@@ -50,21 +90,7 @@ fn main() -> io::Result<()> {
 
                     buf = vec![0.0; (di * dj * dk * dl) as usize];
 
-                    // let start = Instant::now();
                     unsafe {
-                        // int1e_nuc_cart(
-                        //     buf.as_mut_ptr(),
-                        //     std::ptr::null_mut(),
-                        //     shls.as_mut_ptr(),
-                        //     atm.as_mut_ptr(),
-                        //     natm as i32,
-                        //     bas.as_mut_ptr(),
-                        //     nbas as i32,
-                        //     env.as_mut_ptr(),
-                        //     std::ptr::null_mut(),
-                        //     std::ptr::null_mut(),
-                        // );
-
                         cint2e_cart(
                             buf.as_mut_ptr(),
                             shls.as_mut_ptr(),
@@ -73,18 +99,48 @@ fn main() -> io::Result<()> {
                             bas.as_mut_ptr(),
                             nbas as i32,
                             env.as_mut_ptr(),
-                            std::ptr::null_mut()
+                            std::ptr::null_mut(),
                         );
                     }
-                    // let duration = start.elapsed();
-                    // println!("{} {}: {:?}", i, j, duration);
                 }
             }
         }
     }
-    
     let duration_total = start_total.elapsed();
-    println!("{:?}", duration_total);
+    return duration_total;
+}
 
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} file", args[0]);
+        std::process::exit(1);
+    }
+
+    let path = &args[1];
+
+    let mut atm: Vec<i32> = Vec::new();
+    let mut bas: Vec<i32> = Vec::new();
+    let mut env: Vec<f64> = Vec::new();
+
+    read_basis(&path, &mut atm, &mut bas, &mut env);
+
+    let (natm, nbas) = nmol(&mut atm, &mut bas);
+
+    let mut time ;
+    
+    // first call is warmup
+
+    // Nuc integral
+    _ = nuc(&mut atm, natm, &mut bas, nbas, &mut env);
+    time = nuc(&mut atm, natm, &mut bas, nbas, &mut env);
+    println!("{:?} ns", time.as_nanos());
+
+    // Repulsion integral
+    _ = rep(&mut atm, natm, &mut bas, nbas, &mut env);
+    time = rep(&mut atm, natm, &mut bas, nbas, &mut env);
+    println!("{:?} ns", time.as_nanos());
+    
     Ok(())
 }
