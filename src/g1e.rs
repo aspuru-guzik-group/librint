c122cd9be2681c7b2ce71c81de4578afea31c55b
jpmedinagl <jpmedinagl@gmail.com>
2025-06-09 11:51:42 -0400

    cint obj file

--- src/g1e.rs
+++ src/g1e.rs
@@ -1,4 +1,22 @@
 #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
+
+fn MAX<T: PartialOrd>(x: T, y: T) -> T {
+    if x > y {
+        x
+    } else {
+        y
+    }
+}
+
+fn SQUARE(r: *mut f64) -> f64 {
+    unsafe {
+        (*r.add(0) * *r.add(0)) + (*r.add(1) * *r.add(1)) + (*r.add(2) * *r.add(2))
+    }
+}
+
+
+use crate::rys_roots::CINTrys_roots;
+
 extern "C" {
     fn sqrt(_: libc::c_double) -> libc::c_double;
     fn CINTcart_comp(
@@ -9,101 +27,11 @@ extern "C" {
     );
     fn abs(_: libc::c_int) -> libc::c_int;
 }
+
+use crate::cint::CINTEnvVars;
+
 pub type size_t = libc::c_ulong;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct PairData {
-    pub rij: [libc::c_double; 3],
-    pub eij: libc::c_double,
-    pub cceij: libc::c_double,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct CINTOpt {
-    pub index_xyz_array: *mut *mut libc::c_int,
-    pub non0ctr: *mut *mut libc::c_int,
-    pub sortedidx: *mut *mut libc::c_int,
-    pub nbas: libc::c_int,
-    pub log_max_coeff: *mut *mut libc::c_double,
-    pub pairdata: *mut *mut PairData,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct CINTEnvVars {
-    pub atm: *mut libc::c_int,
-    pub bas: *mut libc::c_int,
-    pub env: *mut libc::c_double,
-    pub shls: *mut libc::c_int,
-    pub natm: libc::c_int,
-    pub nbas: libc::c_int,
-    pub i_l: libc::c_int,
-    pub j_l: libc::c_int,
-    pub k_l: libc::c_int,
-    pub l_l: libc::c_int,
-    pub nfi: libc::c_int,
-    pub nfj: libc::c_int,
-    pub c2rust_unnamed: C2RustUnnamed_1,
-    pub c2rust_unnamed_0: C2RustUnnamed_0,
-    pub nf: libc::c_int,
-    pub rys_order: libc::c_int,
-    pub x_ctr: [libc::c_int; 4],
-    pub gbits: libc::c_int,
-    pub ncomp_e1: libc::c_int,
-    pub ncomp_e2: libc::c_int,
-    pub ncomp_tensor: libc::c_int,
-    pub li_ceil: libc::c_int,
-    pub lj_ceil: libc::c_int,
-    pub lk_ceil: libc::c_int,
-    pub ll_ceil: libc::c_int,
-    pub g_stride_i: libc::c_int,
-    pub g_stride_k: libc::c_int,
-    pub g_stride_l: libc::c_int,
-    pub g_stride_j: libc::c_int,
-    pub nrys_roots: libc::c_int,
-    pub g_size: libc::c_int,
-    pub g2d_ijmax: libc::c_int,
-    pub g2d_klmax: libc::c_int,
-    pub common_factor: libc::c_double,
-    pub expcutoff: libc::c_double,
-    pub rirj: [libc::c_double; 3],
-    pub rkrl: [libc::c_double; 3],
-    pub rx_in_rijrx: *mut libc::c_double,
-    pub rx_in_rklrx: *mut libc::c_double,
-    pub ri: *mut libc::c_double,
-    pub rj: *mut libc::c_double,
-    pub rk: *mut libc::c_double,
-    pub c2rust_unnamed_1: C2RustUnnamed,
-    pub f_g0_2e: Option::<unsafe extern "C" fn() -> libc::c_int>,
-    pub f_g0_2d4d: Option::<unsafe extern "C" fn() -> ()>,
-    pub f_gout: Option::<unsafe extern "C" fn() -> ()>,
-    pub opt: *mut CINTOpt,
-    pub idx: *mut libc::c_int,
-    pub ai: [libc::c_double; 1],
-    pub aj: [libc::c_double; 1],
-    pub ak: [libc::c_double; 1],
-    pub al: [libc::c_double; 1],
-    pub fac: [libc::c_double; 1],
-    pub rij: [libc::c_double; 3],
-    pub rkl: [libc::c_double; 3],
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union C2RustUnnamed {
-    pub rl: *mut libc::c_double,
-    pub grids: *mut libc::c_double,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union C2RustUnnamed_0 {
-    pub nfl: libc::c_int,
-    pub ngrids: libc::c_int,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union C2RustUnnamed_1 {
-    pub nfk: libc::c_int,
-    pub grids_offset: libc::c_int,
-}
+
 #[no_mangle]
 pub unsafe extern "C" fn CINTinit_int1e_EnvVars(
     mut envs: *mut CINTEnvVars,
@@ -143,7 +71,7 @@ pub unsafe extern "C" fn CINTinit_int1e_EnvVars(
         (*envs).expcutoff = 60 as libc::c_int as libc::c_double;
     } else {
         (*envs)
-            .expcutoff = MAX(40 as libc::c_int, *env.offset(0 as libc::c_int as isize))
+            .expcutoff = MAX(40 as f64, *env.offset(0 as libc::c_int as isize))
             as libc::c_double;
     }
     (*envs).li_ceil = (*envs).i_l + *ng.offset(0 as libc::c_int as isize);
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 #![feature(extern_types)]
-mod c2f;
-mod cint1e;
-mod cint2e;
-mod cint_bas;
-mod cart2sph;
+
+// pub mod scf;
+
+// pub mod dscf;
+// pub mod p2c;
+
+// pub mod utils;
+// pub mod linalg;
+
+pub mod cint;
+
+pub mod cart2sph;
+pub mod cint1e;
+pub mod cint2e;
+pub mod cint_bas;
+pub mod eigh;
+pub mod fblas;
+pub mod find_roots;
+pub mod fmt;
+pub mod g1e;
+pub mod g2e;
+pub mod intor1;
+pub mod optimizer;
+pub mod rys_roots;
+pub mod rys_wheeler;
